5c4f2f6808bcd32f219ac1777416ab1d43d9346c
Gil Benezer <gil.benezer@gmail.com>
2026-01-04 01:25:04 -0500

    finished the continuous stochastic __init__.py

--- src/systems/builtin/stochastic/continuous/__init__.py
+++ src/systems/builtin/stochastic/continuous/__init__.py
@@ -12,3 +12,52 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from .brownian_motion import BrownianMotion, BrownianMotion2D
+from .geometric_brownian_motion import GeometricBrownianMotion, BrownianMotionWithDrift
+from .ornstein_uhlenbeck import OrnsteinUhlenbeck, MultivariateOrnsteinUhlenbeck
+from .cox_ingersoll_ross import CoxIngersollRoss
+from .langevin_dynamics import LangevinDynamics
+from .stochastic_double_integrator import StochasticDoubleIntegrator
+from .continuous_stochastic_pendulum import ContinuousStochasticPendulum
+from .stochastic_cartpole import StochasticCartPole
+from .stochastic_lorenz import StochasticLorenz
+from .continuous_sir import StochasticSIR
+from .continuous_stochastic_reactors import ContinuousStochasticBatchReactor, ContinuousStochasticCSTR
+
+# Aliases for convenience
+StochasticPendulum = ContinuousStochasticPendulum
+CIR = CoxIngersollRoss
+OU = OrnsteinUhlenbeck
+GBM = GeometricBrownianMotion
+SIR = StochasticSIR
+
+__all__ = [
+    # Brownian motion
+    "BrownianMotion",
+    "BrownianMotion2D",
+    "GeometricBrownianMotion",
+    "BrownianMotionWithDrift",
+    "GBM",
+    # Ornstein-Uhlenbeck process
+    "OrnsteinUhlenbeck",
+    "MultivariateOrnsteinUhlenbeck",
+    "OU",
+    # Cox-Ingersoll-Ross process
+    "CoxIngersollRoss",
+    "CIR",
+    # Langevin dynamics
+    "LangevinDynamics",
+    # Stochastic control systems
+    "StochasticDoubleIntegrator",
+    "ContinuousStochasticPendulum",
+    "StochasticPendulum",
+    "StochasticCartPole",
+    "StochasticLorenz",
+    # Epidemiological models
+    "StochasticSIR",
+    "SIR",
+    # Reactor systems
+    "ContinuousStochasticBatchReactor",
+    "ContinuousStochasticCSTR",
+]
